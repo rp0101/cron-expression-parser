8cb0cd3e0c0e7adc618bac30cb52fbd852a12c5a
rushilparikh <rushil.parikh@clickpost.ai>
2024-02-23 12:36:32 +0530

    added logs for errors in type conversion

--- util/utils.go
+++ util/utils.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"log"
 	"strconv"
 	"strings"
 )
@@ -9,6 +10,7 @@ func StepValues(stepValueStr string, min, max int) string {
 	var values []string
 	step, err := strconv.Atoi(stepValueStr[2:])
 	if err != nil {
+		log.Println("error converting string to int, returning an empty string. Error: ", err.Error())
 		return ""
 	}
 	for i := min; i <= max; i += step {
@@ -30,6 +32,8 @@ func CommaSepValues(commaSepValueStr string, min, max int) string {
 	for _, part := range parts {
 		value, err := strconv.Atoi(part)
 		if err != nil || value < min || value > max {
+			log.Println("error converting string to int or entered value is not in specified range, " +
+				"returning an empty string")
 			return ""
 		}
 		values = append(values, strconv.Itoa(value))
@@ -41,14 +45,19 @@ func RangeValues(rangeValueStr string, min, max int) string {
 	var values []string
 	range_parts := strings.Split(rangeValueStr, "-")
 	if len(range_parts) != 2 {
+		log.Println("A range must have exactly 2 values, returning an empty string.")
 		return ""
 	}
 	first, err := strconv.Atoi(range_parts[0])
 	if err != nil || first < min || first > max {
+		log.Println("error converting string to int or entered value is not in specified range, " +
+			"returning an empty string")
 		return ""
 	}
 	last, err := strconv.Atoi(range_parts[1])
-	if err != nil || last < min || last > max || last < first {
+	if err != nil || first < min || last > max || last < first {
+		log.Println("error converting string to int or entered value is not in specified range, " +
+			"returning an empty string")
 		return ""
 	}
 	for i := first; i <= last; i++ {
